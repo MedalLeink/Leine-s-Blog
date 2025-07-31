0dcc21f304aaf8b3755b9d05826c441a5875412f
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-07-31 05:19:50 -0400

    all folders and files created

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { React, StrictMode } from 'react'
+import { ReactDOM, createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,10 +12,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
-    children: [
+    children:[
       {
         path: "/",
-        element
+        element: <Home/>
       }
     ]
   },
