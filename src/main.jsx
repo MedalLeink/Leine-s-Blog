5b9ad58fd85fcbda9e15a3e7eaabe5b8e88f6f3f
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-07-31 11:53:38 -0400

    routing done

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,55 @@
-import { React, StrictMode } from 'react'
-import { ReactDOM, createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { React, StrictMode } from "react";
+import { ReactDOM, createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import Blogs from "./pages/Blogs.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import About from "./pages/About.jsx";
+import AddPost from "./pages/AddPost.jsx";
+import Post from "./pages/Post.jsx";
+import Contact from "./pages/contact.jsx";
+import Services from "./pages/services.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[
+    element: <App />,
+    children: [
       {
         path: "/",
-        element: <Home/>
+        element: <Home />,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/post",
+        element: <Post />,
+      },
+      {
+        path: "/addpost",
+        element: <AddPost />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
       }
-    ]
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />,
-  </StrictMode>,
-)
+  </StrictMode>
+);
