52ee815cd023f1ca72e8784a7b70394d51d8d8b2
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-07-31 11:58:07 -0400

    NavItems added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Navbar = () => {
 
     const NavItems = [
-        
+       {Path: "/", link: "Home"}, 
+       {Path: "/about", link: "About"}, 
+       {Path: "/post", link: "Post"}, 
+       {Path: "/services", link: "Services"}, 
+       {Path: "/contact", link: "Contact"} 
     ]
   return (
     <div>Navbar</div>
