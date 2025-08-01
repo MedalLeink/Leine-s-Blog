b3de90f03eb5e94bfc8aef9a7f83111da490f304
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-08-01 07:51:47 -0400

    nav links added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 const Navbar = () => {
 
-    const NavItems = [
+    const navItems = [
        {Path: "/", link: "Home"}, 
        {Path: "/about", link: "About"}, 
        {Path: "/post", link: "Post"}, 
@@ -10,11 +11,20 @@ const Navbar = () => {
        {Path: "/contact", link: "Contact"} 
     ]
   return (
-    <head>
-        <nav>
-            <a href="/" className='text-xl font-bold text-white'>Leine's <span>Blog</span></a>
+    <header className='bg-black'>
+        <nav className='px-4 py-4'>
+            <a href="/" className='text-xl font-bold text-indigo-600'>Leine's<span className='text-amber-400'>Blog</span></a>
+            
+            {/* nav items */}
+            <ul>
+                {
+                    navItems.map(({path, link}) => <li>
+                        <NavLink to={path}>{link}</NavLink>
+                    </li>)
+                }
+            </ul>
         </nav>
-    </head>
+    </header>
   )
 }
 
