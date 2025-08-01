bd5f6f56fe6383f0f295d7c3f5cece6b3c9b6878
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-08-01 16:42:04 -0400

    navlinks styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import { NavLink } from 'react-router'
+import React from "react";
+import { NavLink } from "react-router";
+
+// react icons
+import { FaFacebook } from "react-icons/fa6";
 
 const Navbar = () => {
-
-    const navItems = [
-       {Path: "/", link: "Home"}, 
-       {Path: "/about", link: "About"}, 
-       {Path: "/post", link: "Post"}, 
-       {Path: "/services", link: "Services"}, 
-       {Path: "/contact", link: "Contact"} 
-    ]
+  const navItems = [
+    { Path: "/", link: "Home" },
+    { Path: "/about", link: "About" },
+    { Path: "/post", link: "Post" },
+    { Path: "/services", link: "Services" },
+    { Path: "/contact", link: "Contact" },
+  ];
   return (
-    <header className='bg-black'>
-        <nav className='px-4 py-4'>
-            <a href="/" className='text-xl font-bold text-indigo-600'>Leine's<span className='text-amber-400'>Blog</span></a>
-            
-            {/* nav items */}
-            <ul>
-                {
-                    navItems.map(({path, link}) => <li>
-                        <NavLink to={path}>{link}</NavLink>
-                    </li>)
-                }
-            </ul>
-        </nav>
-    </header>
-  )
-}
+    <header className="bg-black">
+      <nav className="px-4 py-4">
+        <a href="/" className="text-xl font-bold text-indigo-600">
+          Leine's<span className="text-amber-400">Blog</span>
+        </a>
 
-export default Navbar
+        {/* nav items */}
+        <ul className="md:flex gap-12 text-lg">
+          {navItems.map(({ path, link }) => (
+            <li className="text-white" key={path}>
+              <NavLink to={path}>{link}</NavLink>
+            </li>
+          ))}
+        </ul>
+
+        {/* menu icons */}
+        <div className="">
+            <a href="/">FaFacebook</a>
+            </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
