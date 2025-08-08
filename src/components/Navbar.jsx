08121b4b6c0e43d60e8465f421bb6d2007bcac0a
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-08-08 19:13:56 -0400

    some bugs fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,11 +17,11 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
   const navItems = [
-    { Path: "/", link: "Home" },
-    { Path: "/about", link: "About" },
-    { Path: "/post", link: "Post" },
-    { Path: "/services", link: "Services" },
-    { Path: "/contact", link: "Contact" },
+    { path: "/", link: "Home" },
+    { path: "/about", link: "About" },
+    { path: "/post", link: "Post" },
+    { path: "/services", link: "Services" },
+    { path: "/contact", link: "Contact" },
   ];
   return (
     <header className="bg-black text-white fixed top-0 left-0 right-0">
@@ -31,7 +31,7 @@ const Navbar = () => {
         </a>
 
         {/* nav items */}
-        <ul className="md:flex gap-12 text-lg hidden">
+        <ul className="md:flex gap-12 text-lg">
           {navItems.map(({ path, link }) => (
             <li className="text-white" key={path}>
               <NavLink to={path}>{link}</NavLink>
@@ -40,7 +40,7 @@ const Navbar = () => {
         </ul>
 
         {/* menu icons */}
-        <div className="hidden text-white lg:flex gap-4 items-center">
+        <div className=" text-white lg:flex gap-4 items-center">
           <a href="/" className="hover:text-amber-400">
             <FaFacebook />
           </a>
@@ -56,7 +56,7 @@ const Navbar = () => {
         </div>
 
         {/* mobile menu btn - display mobile screen */}
-        <button onClick={toggleMenu} className="cursor-pointer">
+        <button onClick={toggleMenu} className="md:hidden cursor-pointer">
           {/* <FaBars className="w-5 h-5 md:hidden" /> */}
           {isMenuOpen ? <FaXmark /> : <FaBars className="w-5 h-5" />}
         </button>
@@ -65,7 +65,7 @@ const Navbar = () => {
       {/* Menu items only for mobile */}
       <div>
         <ul
-          className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${
+          className={`md:hidden gap-12 text-lg space-y-4 px-4 py-6 mt-14 bg-white ${
             isMenuOpen
               ? "fixed top-0 left-0 w-full transition-all ease out duration-150"
               : "hidden"
